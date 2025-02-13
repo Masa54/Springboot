b832c73e2555505d6af463d8e95954b4cdb35618
Masa <mwatanabe@OPS-PC-234.>
2025-02-13 17:54:34 +0900

    first commit

---
+++ backend/springPj/src/test/java/com/example/springPj/SpringPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springPj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
